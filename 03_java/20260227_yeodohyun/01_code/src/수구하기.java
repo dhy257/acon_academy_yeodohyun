0d1d21f9e4d074be52d3d0449e7a16ee6d3e71ed
Yeo Do Hyun <js.dhyeo@gmail.com>
2026-02-27 09:48:26 +0900

    20260227_1교시

---
+++ 03_java/20260227_yeodohyun/01_code/src/수구하기.java
@@ -0,0 +1,30 @@
+
+public class 수구하기 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// 1. 출력자료 : 양수개수: pos_cnt, 음수 개수: neg_cnt, 양수중짝수 개수 : even_cnt,
+		// 양수중 홀수 개수 : odd_cnt
+		// 2. 입력자료 : 입력되는 수 : number
+		// 3. 처리자료 : 반복제어변수: N
+		// 4. Symbolic constant(S.C) : MAX =100
+
+		// 1. MAX만큼 반복
+		// 1.1 수 입력
+		// 1,2 수 판단
+		// 1.2.1 양수이면
+		// -양수 개수 증가
+		// -홀수 짝수 개수 구하기
+		// 1.2.2. 음수이면
+		// -음수 개수 증가
+		// 2. 양수개수, 음수개수 , 양수 중 짝수개수, 양수 중 음수개수 출력
+
+		int pos_cnt = 0, neg_cnt = 0, even_cnt = 0, odd_cnt = 0; // 출력
+		int number; // 입력
+		// 3. 처리자료 : 반복제어변수: i
+		// 4. Symbolic constant(S.C) : MAX =100
+		final int MAX = 5;
+	}
+
+}
