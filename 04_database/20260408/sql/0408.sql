9b4f572c0dbceb49c4b1cd22f532ffe23bc3dc63
Yeo Do Hyun <js.dhyeo@gmail.com>
2026-04-08 11:37:10 +0900

    20260408_3교시

--- 04_database/20260408/sql/0408.sql
+++ 04_database/20260408/sql/0408.sql
@@ -105,6 +105,7 @@ as
     from T_EMP;
 
 select * from t_view;
+drop view t_view;
 
 -- 복합 뷰
 -- 조인쿼리를 뷰로 만들기
@@ -135,5 +136,161 @@ select * from v_emp_dept;
 
 -- 뷰 삭제하기
 drop view v_emp_dept;
+
+-- 상품별 판매수량 조회하기
+--뷰사용해 보기 ( 에이콘 몰에서 조회하기  -상품별 판매수량 조회하기  )
+--뷰 경험하기
+
+select  * from tbl_test_customer;
+select  * from tbl_test_goods;
+select  * from tbl_test_order;
+
+-- 상품별 판매수량 조회하기
+select
+*
+from tbl_test_goods g
+join tbl_test_order o
+on g.pcode = o.pcode;
+
+select
+    g.pname,
+    o.sale_cnt
+from tbl_test_goods g
+join tbl_test_order o
+on g.pcode = o.pcode;
+
+select
+    g.pname 상품명,
+    sum(o.sale_cnt) 판매수량
+from tbl_test_goods g
+join tbl_test_order o
+on g.pcode = o.pcode
+group by g.pname;
+
+CREATE VIEW  V_ORDER
+AS  
+SELECT
+    O.PCODE, G.PNAME,
+    SUM( O.SALE_CNT) AS TOTAL
+FROM TBL_TEST_ORDER O
+JOIN TBL_TEST_GOODS G
+ON O.PCODE = G.PCODE
+GROUP BY O.PCODE, G.PNAME;
+
+select * from v_order;
+
+-- 뷰의 종류
+-- 단순뷰
+-- 복합뷰
+-- 인라인 뷰(FROM 절 뒤에 옴, 임시로 만들어지는 뷰:서브쿼리의 종류)
+
+-- 서브쿼리란? 쿼리안에 쿼리가 있는 형태
+-- 바깥쿼리: 메인쿼리
+-- 안에 있는 쿼리: 서브쿼리
+
+
+-- 서브쿼리 오는 위치
+-- FROM 절 뒤 (인라인 뷰)
+-- WHERE 절 뒤
+-- SELECT 절 뒤 (스칼라 서브쿼리)
+
+-- 1) WHERE 절 뒤 서브쿼리
+-- 서브쿼리의 결과에 따라
+-- 단일행, 단일컬럼
+-- 복수행, 단일컬럼
+
+-- 신동엽 사원보다 급여가 많은 사람 이름 조회하시오.
+select * from t_emp;
+
+-- 1. 신동엽의 sal 조회
+select
+    salary
+from t_emp
+where ename = '신동엽';
+
+select
+    ename, salary
+from t_emp
+where ename = '신동엽';
+
+select
+    ename
+from t_emp
+where salary>280;
+
+-- 서브쿼리 사용
+-- 두 쿼리 합체
+
+-- 연관 서브쿼리 : 서브쿼리에서 메인쿼리의 내용을 사용하는 것
+-- 비연관 서브쿼리 : 서브쿼리 단독 사용
+
+select
+    ename
+from t_emp
+where salary>(
+    select salary
+    from t_emp
+    where ename = '신동엽'
+);
+
+-- 에이콘 테이블
+select * from acorntbl;
+
+-- 가장 어린 사람 이름 조회
+select max(birthday) from acorntbl;
+select min(birthday) from acorntbl;
+
+select
+    name,
+    birthday
+from acorntbl;
+
+select
+    name, birthday
+from acorntbl
+where birthday = '05/01/25';
+
+select
+    name, birthday
+from acorntbl
+where birthday = (
+    select max(birthday) from acorntbl
+);
+
+--단일행 서브쿼리
+   --서브쿼리의 결과가 단일행 , 단일컬럼 ( 서브쿼리의 결과가  값 하나일 때)
+   --단일행 서브쿼리 연산 = , >, < ,>= ,< =  연산자 사용가능하다
+
+
+-- 문제)
+-- acorntbl 테이블에서 포인트가 가장 높은 사람의 이름 조회
+select
+    name, point
+from acorntbl
+where point = (
+    select max(point) from acorntbl
+);
+
+-- acorntbl 테이블에서 포인트의 평균보다 이상인 사람 이름 조회
+select
+    name, point
+from acorntbl
+where point > (
+    select avg(point) from acorntbl
+);
+
+
+-- emp테이블에서 가장 높은 급여 받는 사람 이름 조회
+select
+    ename, sal
+from emp
+where sal = (
+    select max(sal) from emp
+);
+
+
+
+
+
 
 
