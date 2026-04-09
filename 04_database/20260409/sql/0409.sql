5ff16639f13795e0274d7070cf7a8f6491c4e265
Yeo Do Hyun <js.dhyeo@gmail.com>
2026-04-09 14:17:43 +0900

    20260409_3,4교시

--- 04_database/20260409/sql/0409.sql
+++ 04_database/20260409/sql/0409.sql
@@ -160,14 +160,231 @@ order by 1;
 select * from student;
 select * from professor;
 
-
-
-
-
-
-
-
-
+select
+    s.name,
+    nvl(p.name,' ')
+from student s
+left outer join professor p
+on s.profno = p.profno;
 
 -- 사용예2
+select
+    nvl(s.name,' '),
+    p.name
+from student s
+right outer join professor p
+on s.profno = p.profno;
+
+
+-- 교재250p
+-- self join
+-- 자기자신과 조인
+
+select * from emp;
+
+select *
+from emp e1
+join emp e2
+on e1.mgr = e2.empno;
+
+select
+    e1.empno,
+    e1.ename,
+    e2.ename
+from emp e1
+join emp e2
+on e1.mgr = e2.empno;
+
+select
+    e1.empno,
+    e1.ename,
+    nvl(to_char(e1.mgr),' ') MGR
+from emp e1
+left outer join emp e2
+on e1.mgr = e2.empno
+order by 1;
+
+-- join을 쉽게 하려렴
+-- 1. 각 테이블 조회
+-- 2. 눈으로 조인
+-- 3. 조인하기, 수행하기 - 조인조건
+-- 4. 필요하면 (INNER 조인에 나오지 않은 데이터) OUTER로 바꾸기 - LEFT, RIGHT, FULL
+
+-- cross join
+-- 조인조건이 없는 조인
+SELECT * FROM tbl_test_customer; -- 7
+SELECT * FROM tbl_test_goods;
+SELECT * FROM tbl_test_order; -- 8
+
+select *
+from tbl_test_order o
+join tbl_test_customer c
+on o.id = c.id;
+
+-- 각 테이블행의 개수 * 행수의 개수 만큼의 조합이 만들어짐
+-- 8*7 =56
+select *
+from tbl_test_order o
+cross join tbl_test_customer c;
+
+-- 
+CREATE TABLE drink_order (
+    order_id NUMBER,
+    drink_name VARCHAR2(20)
+);
+ 
+CREATE TABLE drink_order_option (
+    order_id NUMBER,
+    option_name VARCHAR2(20)
+);
+ 
+ 
+ -- 주문
+INSERT INTO drink_order VALUES (1, '아메리카노');
+INSERT INTO drink_order VALUES (2, '라떼');
+
+-- 옵션
+INSERT INTO drink_order_option VALUES (1, '샷 추가');
+INSERT INTO drink_order_option VALUES (1, '얼음 적게');
+INSERT INTO drink_order_option VALUES (1, '시럽 추가');
+
+INSERT INTO drink_order_option VALUES (2, '우유 변경');
+INSERT INTO drink_order_option VALUES (2, '휘핑 추가');
+
+COMMIT;
+--
+
+select * from drink_order;
+select * from drink_order_option;
+
+select 
+    d.order_id,
+    d.drink_name,
+    o.option_name
+from drink_order d
+join drink_order_option o
+on d.order_id = o.order_id;
+
+select 
+    d.order_id,
+    d.drink_name,
+    o.option_name
+from drink_order d
+join drink_order_option o
+on d.order_id = o.order_id;
+
+select 
+    d.order_id,
+    d.drink_name,
+    listagg(o.option_name, ', ' )
+    within group( order by o.option_name )
+from drink_order d
+join drink_order_option o
+on d.order_id = o.order_id
+group by d.order_id, d.drink_name;
+
+
+
+-- 연습문제
+-- 교재 254p
+
+-- 1, 2, 3(나이 값이 다를거임), 4(결과 5개)
+-- 1.
+select * from student;
+select * from department;
+
+select *
+from student s
+join department d
+on s.deptno1 = d.deptno;
+
+select
+    s.name STU_NAME,
+    s.deptno1,
+    d.dname DEPT_NAME
+from student s
+join department d
+on s.deptno1 = d.deptno;
+
+-- 2.
+select * from emp2;
+select * from p_grade;
+
+select *
+from emp2 e
+right outer join p_grade p
+on e.position = p.position;
+
+select
+    e.name name,
+    p.position,
+    to_char(e.pay,'999,999,999') pay,
+    to_char(p.s_pay,'999,999,999') "Low Pay",
+    to_char(p.e_pay,'999,999,999') "High Pay"
+from emp2 e
+right outer join p_grade p
+on e.position = p.position
+where pay is not null;
+
+-- 3.
+select * from emp2;
+select * from p_grade;
+
+select *
+from emp2 e
+left outer join p_grade p
+on e.position = p.position;
+
+select
+    e.name,
+    e.birthday,
+    e.position
+from emp2 e
+left outer join p_grade p
+on e.position = p.position;
+
+select
+    e.name,
+    to_char(e.birthday,'YY'),
+    e.position
+from emp2 e
+left outer join p_grade p
+on e.position = p.position;
+
+select
+    e.name,
+    trunc(months_between(sysdate, birthday)/12) age,
+    e.position curr_position
+from emp2 e
+left outer join p_grade p
+on e.position = p.position
+order by 2;
+
+select
+    e.name,
+    trunc(months_between(sysdate, birthday)/12) age,
+    nvl(e.position,' ') curr_position,
+    nvl(p.position,' ') be_position
+from emp2 e
+left outer join p_grade p
+on
+trunc(months_between(sysdate, birthday)/12) >= p.s_age
+and
+trunc(months_between(sysdate, birthday)/12) <= p.e_age
+order by 2;
+
+-- 4.
+select * from customer;
+select * from gift;
+
+select
+    c.gname,
+    c.point,
+    g.gname
+from customer c
+join gift g
+on
+    c.point >= g.g_start
+where g.gname like 'Notebook';
+
 
