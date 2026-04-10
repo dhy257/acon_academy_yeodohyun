d19e918c32f57191e17e5da00f0f11fd274a98a1
Yeo Do Hyun <js.dhyeo@gmail.com>
2026-04-10 11:46:21 +0900

    20260410_3교시

--- 04_database/20260410/sql/0410.sql
+++ 04_database/20260410/sql/0410.sql
@@ -90,7 +90,7 @@ drop table member_test;
 -- 교재285p
 -- 1.
 create table new_emp(
-    no number(5),
+    no number(5) primary key,
     nane varchar(20),
     hiredate date,
     bonus number(6,2)
@@ -136,10 +136,95 @@ select * from new_emp2;
 drop table new_emp2;
 
 
+-- 키의 종류 
+-- 슈퍼키 :  유일성 만족
+-- 후보키 : 주키가 될 수 있는 후보  (유일성  +  최소성)
+-- 주키 : 후보키중에 선택된 키
+-- 대체키 : 후보키중에 나머지 키
+-- 외래키 : 다른 테이블의 주키로써 관계성 설정을 위해서 가지고 있는 키
 
 
+--학생테이블 
+--학번  이름    학년  반 번호
+--001   이순신   1   1  10
+--002   홍길동   1   1  9
+--003   이순신   2   3  4
+
+-- 슈퍼키 (유일성)
+-- 학번포함 다 됨
+
+-- 후보키 (유일성 + 최소성)
+-- 학번
+-- 학년 + 반 + 번호
+
+-- 기본키 : 학번
+-- 대체키 : 학년 + 반 + 번호
+
+-- 학번, 이름, 학년, 반, 번호 => 한 학생을 유일하게 식별가능한가? => 가능하다 => 유일성 만족 => 슈퍼키 가능 / 최소성 위배
+-- 학번, 이름, 학년, 반 => 유일성 만족 => 슈퍼키 가능 / 최소성 위배
+-- 학번, 이름, 학년 => 유일성 만족 => 슈퍼키 가능 / 최소성 위배
+-- 학번, 이름 => 유일성 만족 / 최소성 위배
+-- 이름 => 유일성 불만족 => 슈퍼키 불가능 / 최소성 위배
+-- 학년, 반, 번호 => 유일성 만족 => 슈퍼키 가능 / 최소성 만족(학년,반,번호 가 고정되야 한 학생 픽스가능) => 후보키
+-- 학번 : 이것만 있어도 됨 / 최소성 만족 => 후보키
+
+-- 1) 유일성 : 키의 조합으로 유일하게 하나의 레코드를 식별할 수 있는지 여부
+-- 2) 최소성 : 키의 조합이 최소한 존재해야지만 키로 사용하는것이 가능한가?
+--          : 키의 일부로 유일하게 식별이 가능하다면 최소성 위배가 된다
+--          : 최소성을 만족해야 후보키 가능하다
 
 
+-- 제약조건 교재324p
+
+-- 도메인제약조건
+-- 하나의 컬럼이 가지는 제약조건
+CREATE TABLE TEST_2021(
+   ID VARCHAR2(7)  NOT NULL, 
+   EMAIL VARCHAR2(50) NULL,
+   PHONE CHAR(13) NOT NULL,
+   PWD VARCHAR2(7) DEFAULT '1111'
+);
+select * from test_2021;
+
+-- 데이터 추가하기
+insert into test_2021 ( id,phone ) values ( 'test','0101231234' );
+commit;
+
+-- not null 테스트하기
+-- 필수입력사항들은 not null
+-- ORA-01400: NULL을 ("SCOTT"."TEST_2021"."PHONE") 안에 삽입할 수 없습니다
+insert into test_2021 ( id ) values ( 'test2' );
+commit;
+
+--CHECK 제약 조건
+-- '010-%-____' _4개임 >> 4개쓰라고
+CREATE  TABLE TEST_2021_2(
+   ID VARCHAR2(50)  , 
+   PHONE VARCHAR2(20) CHECK ( PHONE  LIKE  '010-%-____') NOT NULL,
+   EMAIL VARCHAR2(50)  NULL
+);
+
+insert into test_2021_2 (id,phone) values ('test2','010-3903-1028');
+commit;
+select * from test_2021_2;
+
+-- ORA-02290: 체크 제약조건(SCOTT.SYS_C007556)이 위배되었습니다
+insert into test_2021_2 (id,phone) values ('test3','01011112222');
+insert into test_2021_2 (id,phone) values ('test4','010-3903-123');
+
+CREATE  TABLE TEST_2021_22(
+   ID VARCHAR2(50)  , 
+   PHONE VARCHAR2(20) CHECK ( PHONE  LIKE  '010-%-____') NOT NULL,
+   EMAIL VARCHAR2(50)  NULL,
+   GRADE CHAR(2) CHECK( GRADE IN ('01','02','03'))
+);
+
+select * from test_2021_22;
+
+INSERT INTO  TEST_2021_22  (ID, PHONE, GRADE)  VALUES('TEST3' ,  '010-3903-1234','01');
+commit;
+
+INSERT INTO  TEST_2021_22  (ID, PHONE, GRADE)  VALUES('TEST3' ,  '010-3903-1234','01');
 
 
 
