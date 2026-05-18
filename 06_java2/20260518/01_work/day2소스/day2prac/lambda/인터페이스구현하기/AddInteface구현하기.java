e4cb672f7ce6fd9b18d49773fcdb84131f46c4bb
Yeo Do Hyun <js.dhyeo@gmail.com>
2026-05-18 10:47:51 +0900

    20260518_2교시

--- 06_java2/20260518/01_work/day2소스/day2prac/lambda/인터페이스구현하기/AddInteface구현하기.java
+++ 06_java2/20260518/01_work/day2소스/day2prac/lambda/인터페이스구현하기/AddInteface구현하기.java
@@ -16,7 +16,7 @@ public class AddInteface구현하기 {
 		// 3. 람다식 구현 ( 익명클래스에서 추론 가능한 모든 코드를 생략하고 사용 )
 
 		// 1. 지역내부클래스
-		class A implements AddInteface {
+		class A implements AddInterface {
 			@Override
 			public int add(int x, int y) {
 				// TODO Auto-generated method stub
@@ -24,14 +24,14 @@ public class AddInteface구현하기 {
 			}
 		}
 
-		AddInteface f1 = new A();
+		AddInterface f1 = new A();
 		int result = f1.add(5, 3);
 		System.out.println(result);
 
 		// 2. 익명클래스 구현
 		// AddInteface f2 = new AddInteface() {};
 		// 익명클래스 구현, 생성된 클래스로 객체생성하기 new
-		AddInteface f2 = new AddInteface() {
+		AddInterface f2 = new AddInterface() {
 
 			@Override
 			public int add(int x, int y) {
@@ -44,13 +44,13 @@ public class AddInteface구현하기 {
 		System.out.println(result2);
 
 		// 3. 람다식
-		AddInteface f3 = (x, y) -> {
+		AddInterface f3 = (x, y) -> {
 			return x + y;
 		};
 		int result3 = f3.add(5, 11);
 		System.out.println(result3);
 
-		AddInteface f4 = (x, y) -> x + y;
+		AddInterface f4 = (x, y) -> x + y;
 		int result4 = f4.add(7, 7);
 		System.out.println(result4);
 
--- 06_java2/20260518/01_work/day2소스/day2prac/lambda/인터페이스구현하기/AddInterface.java
+++ 06_java2/20260518/01_work/day2소스/day2prac/lambda/인터페이스구현하기/AddInterface.java
@@ -1,6 +1,6 @@
 package day2prac.lambda.인터페이스구현하기;
 
 // 구현해야할 기능이 한 개인 인터페이스 만들기
-public interface AddInteface {
+public interface AddInterface {
 	int add(int x, int y);
 }
